76f65c5837e951c3face8d055c4ef896a0088f83
IharMrachko <ihormrochko@gmail.com>
2018-05-28 21:00:34 +0300

    My first git commit

---
+++ src/main/java/education/by/traineng/TrainengApplication.java
@@ -0,0 +1,12 @@
+package education.by.traineng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainengApplication.class, args);
+	}
+}
