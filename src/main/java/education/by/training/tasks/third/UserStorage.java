7c7aefd43af67ee4de61e0485f9a88473509d3ca
Artem Slednev <artsiom.slednev@gmail.com>
2018-05-30 22:32:00 +0300

    third task

---
+++ src/main/java/education/by/training/tasks/third/UserStorage.java
@@ -0,0 +1,19 @@
+package education.by.training.tasks.third;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserStorage {
+
+    private Map<String, User> users = new HashMap<String, User>(100);
+
+
+    public User findUser(String id){
+
+    }
+
+
+    public void generateUser(){
+
+    }
+}
