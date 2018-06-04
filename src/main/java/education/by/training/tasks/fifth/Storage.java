1852d3ab840d03af4dc9630adfe06721dcbd0f57
Artem Slednev <artsiom.slednev@gmail.com>
2018-06-04 20:50:38 +0300

    fifth

--- src/main/java/education/by/training/tasks/fifth/Storage.java
+++ src/main/java/education/by/training/tasks/fifth/Storage.java
@@ -1,4 +1,28 @@
 package education.by.training.tasks.fifth;
 
+import java.util.HashMap;
+
 public class Storage {
+
+
+    private Map<String, Role> roleStorage = new HashMap<>();
+    private Map<String, User> userStorage = new HashMap<>();
+
+    public void fillRoleStorage(){
+
+    }
+
+
+    public void fillUserStorage(){
+
+    }
+
+
+    public void linkUserToRole(){
+
+    }
+
+    public User getUser(String userId){
+
+    }
 }
---
+++ src/main/java/education/by/training/tasks/fifth/fifth.md
@@ -0,0 +1,14 @@
+Класс Storage:
+
+--
+
+класс Role имеет id, name, userId
+класс User имеет id, name, Role
+
+--
+
+
+1. метод fillRoleStorage должен заполнять map roleStorage
+2. метод fillUserStorage должен заполнять map userStorage
+3. метод linkUserToRole должен сетить роли к юзерам из map
+4. метод getUser должен возвращать юзера по id
